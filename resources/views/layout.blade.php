757abf54cadf6a8f65bfc29a4ed537f7103ad22d
Dav-Olobo <davidolobo10@gmail.com>
2025-08-22 16:41:11 +0100

    Final ref

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="{{ route('products.index') }}">MyShop</a>
+            <a class="navbar-brand" href="{{ route('products.index') }}">MyCRUD</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
